b0d26b8551edbdc4c62999ce9eaa8ac9adad82d4
Nadia Bahrami <nadia.bahrami@gmail.com>
2015-12-21 15:10:53 -0800

    Array of checks working.  Coordinate array of selected and google map integration needed

--- js/app.js
+++ js/app.js
@@ -47,13 +47,13 @@ function Stadium (name, city, team, long, lat){
   }
 })();
 
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 38.0, lng: -95.35},
-    zoom: 4
-  });
-}
+//var map;
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: 38.0, lng: -95.35},
+//     zoom: 4
+//   });
+// }
 
 var inputHandler = {
   listContainer: document.getElementById('list'),
@@ -94,13 +94,71 @@ var userSelects = {
       }
     };
     console.log (checked);
+    var coordinates =[];
+    for(var j=0; j<checked.length; j++){
+
+    }
   },
 
 };
+
+function initMap() {
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 38.0, lng: -95.35},
+    zoom: 4
+  });
+  directionsDisplay.setMap(map);
+
+  //document.getElementById('submit').addEventListener('click', function() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+//  });
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  var waypts = [];
+  var checkboxArray = document.getElementById('waypoints');
+  for (var i = 0; i < checkboxArray.length; i++) {
+    if (checkboxArray.options[i].selected) {
+      waypts.push({
+        location: checkboxArray[i].value,
+        stopover: true
+      });
+    }
+  }
+
+  directionsService.route({
+    origin: document.getElementById('start').value,
+    destination: document.getElementById('end').value,
+    waypoints: waypts,
+    optimizeWaypoints: true,
+    travelMode: google.maps.TravelMode.DRIVING
+  }, function(response, status) {
+    if (status === google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+      var route = response.routes[0];
+      var summaryPanel = document.getElementById('directions-panel');
+      summaryPanel.innerHTML = '';
+      // For each route, display summary information.
+      for (var i = 0; i < route.legs.length; i++) {
+        var routeSegment = i + 1;
+        summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+            '</b><br>';
+        summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+        summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+        summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+      }
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+}
 
 userSelects.planTrip.addEventListener('click', function(event){
   event.preventDefault();
   console.log("It works");
   userSelects.checkboxCheck();
+  initMap();
 
 });
